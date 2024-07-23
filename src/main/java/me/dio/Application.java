064fd83f73c670655684913e25d3209cf63e9125
sminato27 <listenandread2708@gmail.com>
2024-07-23 13:53:58 -0300

    .

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -6,15 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 
-/**
- * Initializes our RESTful API.
- * 
- * <p>
- * The {@link OpenAPIDefinition} annotation was used to enable HTTPS in the Swagger UI.
- * For more details, see the following post on Stack Overflow: 
- * https://stackoverflow.com/a/71132608/3072570
- * </p>
- */
+
 
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
